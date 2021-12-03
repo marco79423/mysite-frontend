53e41d90f1a9164f4000bf00a73aaf8ac3999276
marco79423 <marco79423@gmail.com>
2021-12-04 01:21:30 +0800

    feat(diudiu-frontend): 完成骰子

--- src/apps/diudiu-frontend/pages/index.js
+++ src/apps/diudiu-frontend/pages/index.js
@@ -1,55 +1,83 @@
 import React from 'react'
 import Head from 'next/head'
-import {Canvas, useFrame} from '@react-three/fiber'
+import {Canvas, extend, useFrame, useThree} from '@react-three/fiber'
 import {CssBaseline} from '@material-ui/core'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 import useWindowSize from '../components/hooks/useWindowSize'
+
+extend({OrbitControls})
 
 export default function Index() {
   const {width, height} = useWindowSize()
 
   return (<>
-      <Head>
-        <title>丟丟</title>
-        <meta name="description" content="丟丟"/>
-        <link rel="icon" href="/favicon.ico"/>
-      </Head>
+    <Head>
+      <title>丟丟</title>
+      <meta name="description" content="丟丟"/>
+      <link rel="icon" href="/favicon.ico"/>
+    </Head>
 
-      <CssBaseline/>
+    <CssBaseline/>
 
-      <DiceBox width={width} height={height}/>
-    </>)
+    <DiceBox width={width} height={height}/>
+  </>)
+}
+
+function CameraControls() {
+  // Get a reference to the Three.js Camera, and the canvas html element.
+  // We need these to setup the OrbitControls component.
+  // https://threejs.org/docs/#examples/en/controls/OrbitControls
+  const {camera, gl: {domElement}} = useThree()
+
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = React.useRef()
+  useFrame((state) => controls.current.update())
+  return <orbitControls ref={controls} args={[camera, domElement]}/>
 }
 
 function DiceBox({width, height}) {
-  return (<Canvas style={{background: 'blue', height, width}}>
-      <ambientLight/>
-      <pointLight position={[10, 10, 10]}/>
-      <Box position={[-1.2, 0, 0]}/>
-      <Box position={[1.2, 0, 0]}/>
-    </Canvas>)
+  return (
+    <Canvas style={{height, width}} shadows>
+      <CameraControls/>
+      <axesHelper/>
+      <ambientLight color={0xf0f5fb}/>
+      <spotLight
+        color={0xefdfd5}
+        intensity={20}
+        position={[-2, 2, 2]}
+        distance={5}
+        castShadow={true}
+      />
+
+      <Desk/>
+      <Dice/>
+    </Canvas>
+  )
 }
 
-
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
+function Desk(props) {
   const ref = React.useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = React.useState(false)
-  const [clicked, click] = React.useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (<mesh
+
+  return (
+    <mesh
       {...props}
       ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]}/>
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'}/>
-    </mesh>)
+      receiveShadow={true}
+    >
+      <planeGeometry args={[5, 5]}/>
+      <meshPhongMaterial color={0xdfdfdf}/>
+    </mesh>
+  )
 }
 
+function Dice(props) {
+  const ref = React.useRef()
 
+  return (
+    <mesh ref={ref} castShadow={true} position={[0, 0, 0.5]}>
+      <boxGeometry args={[1, 1, 1]}/>
+      <meshStandardMaterial color={'#202020'}/>
+    </mesh>
+  )
+}
