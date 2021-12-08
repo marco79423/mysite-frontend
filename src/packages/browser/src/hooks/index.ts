b5feeffc50e748c1bb312b593580326e80ee7d0b
Marco Li <marco_li@paradise-soft.com.tw>
2021-12-08 11:49:44 +0800

    chore(paji-sdk): 新增 paji-sdk.js

---
+++ src/packages/browser/src/hooks/index.ts
@@ -0,0 +1,2 @@
+export {default as useViewportSize} from './useViewportSize'
+export {default as useComponentSize} from './useComponentSize'
---
+++ src/packages/utils/src/generateRandomString.js
@@ -0,0 +1,20 @@
+const DEFAULT_LENGTH = 6
+const DEFAULT_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+/**
+ * 產生随機亂數
+ * @param {object=} config - 參數設定
+ * @param {number=} config.length - 字串長度
+ * @param {number=} config.charset - 字元集
+ * @returns {string}
+ */
+export default function generateRandomString(config = {}) {
+  const {length = DEFAULT_LENGTH, charset = DEFAULT_CHARSET} = config || {}
+
+  let result = ''
+  const charsetLength = charset.length
+  for (let i = 0; i < length; i++) {
+    result += charset.charAt(Math.floor(Math.random() * charsetLength))
+  }
+  return result
+}
