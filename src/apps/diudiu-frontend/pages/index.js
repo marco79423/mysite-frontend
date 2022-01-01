36660c04f0ae7fb076b6c06fc5f336ad7a273756
Marco Li <marco_li@paradise-soft.com.tw>
2022-01-02 00:01:20 +0800

    新增可以手動骰的功能

--- src/apps/diudiu-frontend/pages/index.js
+++ src/apps/diudiu-frontend/pages/index.js
@@ -2,19 +2,23 @@ import React, {useRef} from 'react'
 import Head from 'next/head'
 import {Canvas} from '@react-three/fiber'
 import {Physics, useBox, usePlane} from '@react-three/cannon'
-import {CssBaseline} from '@material-ui/core'
 
 import useWindowSize from '../components/hooks/useWindowSize'
 import {OrbitControls, PerspectiveCamera, RoundedBox, useHelper} from '@react-three/drei'
 import {CameraHelper} from 'three'
+import {Button, CssBaseline, Grid} from '@mui/material'
 
 export default function Index() {
   const {width, height} = useWindowSize()
+
 
   return (<>
     <Head>
       <title>丟丟</title>
       <meta name="description" content="丟丟"/>
+      <meta name="viewport" content="initial-scale=1, width=device-width"/>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
       <link rel="icon" href="/favicon.ico"/>
     </Head>
 
@@ -25,37 +29,51 @@ export default function Index() {
 }
 
 function DiceBox({width, height}) {
+  const [dice, setDice] = React.useState([])
+
+  const addDie = () => {
+    setDice([
+      ...dice,
+      <Die position={[0, -3.5, 5]} velocity={[20 * Math.random() - 20 * Math.random(), 20 + 20 * Math.random(), -20 * Math.random()]}/>
+    ])
+  }
+
   return (
-    <Canvas
-      style={{height, width, background: 'lightblue'}}
-      shadows
-    >
-      <Physics gravity={[0, 0, -10]} defaultContactMaterial={{friction: 0.01, restitution: 0.5}}>
-        <OrbitControls/>
-        <PerspectiveCamera makeDefault position={[0, 0, 10]}/>
+    <main style={{width, height}}>
+      <div style={{display: 'relative', position: 'absolute', width, height}}>
+        <Grid container alignItems="center" justifyContent="center" style={{height: '100%'}}>
+          <Grid item>
+            <Button onClick={addDie} variant="contained" style={{zIndex: 9, fontSize: '3rem'}}>丟</Button>
+          </Grid>
+        </Grid>
+      </div>
 
-        <axesHelper/>
-        <ambientLight color={0xf0f5fb}/>
-        <spotLight
-          color={0xefdfd5}
-          intensity={20}
-          position={[-2, 2, 2]}
-          distance={5}
-          castShadow={true}
-        />
+      <Canvas style={{position: 'absolute', height, width, background: 'lightblue'}} shadows>
+        <Physics gravity={[0, 0, -10]} defaultContactMaterial={{friction: 0.01, restitution: 0.5}}>
+          <OrbitControls/>
+          <PerspectiveCamera makeDefault position={[0, 0, 10]}/>
 
-        <Desk/>
+          <axesHelper/>
+          <ambientLight color={0xf0f5fb}/>
+          <spotLight
+            color={0xefdfd5}
+            intensity={20}
+            position={[-2, 2, 2]}
+            distance={5}
+            castShadow={true}
+          />
 
-        <BarrierTop/>
-        <BarrierBottom/>
-        <BarrierLeft/>
-        <BarrierRight/>
+          <Desk/>
 
-        <Dice position={[3.5, 0, 5]} velocity={[-5, 0, 0]}/>
-        <Dice position={[-3.5, 0, 5]} velocity={[15, 0, 0]}/>
-        <Dice position={[0, -3.5, 5]} velocity={[0, 20, 0]}/>
-      </Physics>
-    </Canvas>
+          <BarrierTop/>
+          <BarrierBottom/>
+          <BarrierLeft/>
+          <BarrierRight/>
+
+          {dice}
+        </Physics>
+      </Canvas>
+    </main>
   )
 }
 
@@ -74,7 +92,7 @@ function Desk(props) {
   )
 }
 
-function Dice({position, velocity, ...props}) {
+function Die({position, velocity, ...props}) {
   const [ref, api] = useBox(() => ({mass: 1, position: position, velocity: velocity}))
 
   return (
