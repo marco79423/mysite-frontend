7d39fa2854aabdda725f4e58c1e3f9e62f813550
marco79423 <marco79423@gmail.com>
2022-01-21 22:22:18 +0800

    feat(diudiu-frontend): 新增開發者模式

--- src/apps/diudiu-frontend/pages/index.js
+++ src/apps/diudiu-frontend/pages/index.js
@@ -12,13 +12,14 @@ import {
   CssBaseline,
   Dialog, DialogActions, DialogContent,
   DialogTitle,
-  Fab,
+  Fab, FormControlLabel, FormGroup, Switch,
   Toolbar,
   Typography
 } from '@mui/material'
 import {createTheme, ThemeProvider} from '@mui/material/styles'
 import CasinoIcon from '@mui/icons-material/Casino'
 import {orange} from '@mui/material/colors'
+import {createGlobalState, useList, useToggle} from 'react-use'
 
 const theme = createTheme({
   palette: {
@@ -30,28 +31,35 @@ const theme = createTheme({
   },
 })
 
+const useDeveloperMode = createGlobalState(false)
+
+
 export default function Index() {
   const {width, height} = useWindowSize()
-  const [dice, setDice] = React.useState([])
-  const [open, setOpen] = React.useState(false);
+  const [dice, {push, clear}] = useList()
+
+  const [on, toggle] = useToggle(false)
+  const [developerMode, setDeveloperMode] = useDeveloperMode()
 
   const dieCount = dice.length
 
   const addDie = () => {
-    setDice([...dice, <Die position={[0, -3.5, 5]}
-                           velocity={[20 * Math.random() - 20 * Math.random(), 20 + 20 * Math.random(), -20 * Math.random()]}/>])
+    push(<Die
+      position={[0, -3.5, 5]}
+      velocity={[20 * Math.random() - 20 * Math.random(), 20 + 20 * Math.random(), -20 * Math.random()]}
+    />)
   }
 
   const showDialog = () => {
-    setOpen(true)
+    toggle(true)
   }
 
   const hideDialog = () => {
-    setOpen(false)
+    toggle(false)
   }
 
   const clearDice = () => {
-    setDice([])
+    clear()
     hideDialog()
   }
 
@@ -99,6 +107,11 @@ export default function Index() {
                 {/*</Link>*/}
               </nav>
               <Box sx={{flexGrow: 1}}/>
+              <FormGroup>
+                <FormControlLabel control={<Switch color="secondary" checked={developerMode}
+                                                   onChange={(e) => setDeveloperMode(e.target.checked)}/>}
+                                  label="開發者模式"/>
+              </FormGroup>
               <Button href="#" variant="outlined" sx={{my: 1, mx: 1.5}}>
                 我的
               </Button>
@@ -114,7 +127,8 @@ export default function Index() {
             }}>
             <Box sx={{flexGrow: 1}}/>
             <Box sx={{flex: 1, position: 'relative'}}>
-              <Fab color="primary" aria-label="丟" onClick={addDie} style={{zIndex: 9, fontSize: '3rem', width: 80, height: 80, userSelect: 'none'}}>
+              <Fab color="primary" aria-label="丟" onClick={addDie}
+                   style={{zIndex: 9, fontSize: '3rem', width: 80, height: 80, userSelect: 'none'}}>
                 丟
               </Fab>
 
@@ -123,8 +137,8 @@ export default function Index() {
                 統計
               </Fab>
 
-              <Dialog onClose={hideDialog} open={open}>
-                <DialogTitle sx={{ m: 0, p: 2 }}>統計</DialogTitle>
+              <Dialog onClose={hideDialog} open={on}>
+                <DialogTitle sx={{m: 0, p: 2}}>統計</DialogTitle>
                 <DialogContent dividers>
                   <Typography>你丟了： {dieCount} 次</Typography>
                 </DialogContent>
@@ -137,10 +151,10 @@ export default function Index() {
           </Box>
           <Canvas style={{position: 'absolute', height, width, background: 'lightblue'}} shadows>
             <Physics gravity={[0, 0, -10]} defaultContactMaterial={{friction: 0.01, restitution: 0.5}}>
-              <OrbitControls/>
-              <PerspectiveCamera makeDefault position={[0, 0, 10]}/>
+              {developerMode && <OrbitControls/>}
+              {developerMode && <axesHelper/>}
 
-              <axesHelper/>
+              <PerspectiveCamera makeDefault position={[0, 0, 10]}/>
               <ambientLight color={0xf0f5fb}/>
               <spotLight
                 color={0xefdfd5}
@@ -169,18 +183,16 @@ export default function Index() {
 function Desk(props) {
   const [ref] = usePlane(() => ({...props}))
 
-  return (<mesh
-    {...props}
-    ref={ref}
-    receiveShadow={true}
-  >
-    <planeGeometry args={[8, 8]}/>
-    <meshPhongMaterial color={0xdfdfdf}/>
-  </mesh>)
+  return (
+    <mesh{...props} ref={ref} receiveShadow={true}>
+      <planeGeometry args={[8, 8]}/>
+      <meshPhongMaterial color={0xdfdfdf}/>
+    </mesh>
+  )
 }
 
 function Die({position, velocity, ...props}) {
-  const [ref, api] = useBox(() => ({mass: 1, position: position, velocity: velocity}))
+  const [ref] = useBox(() => ({mass: 1, position: position, velocity: velocity}))
 
   return (<RoundedBox
     {...props}
@@ -196,48 +208,48 @@ function Die({position, velocity, ...props}) {
 
 function BarrierTop(props) {
   const [ref] = usePlane(() => ({position: [0, 4, 4], rotation: [Math.PI / 2, 0, 0]}))
+  const [developerMode] = useDeveloperMode()
 
-  return (<mesh
-    {...props}
-    ref={ref}
-  >
-    <planeGeometry args={[8, 8]}/>
-    <meshPhongMaterial color={'blue'}/>
-  </mesh>)
+  return (
+    <mesh{...props} ref={ref}>
+      <planeGeometry args={[8, 8]}/>
+      <meshPhongMaterial color={developerMode ? 'blue' : null}/>
+    </mesh>
+  )
 }
 
 function BarrierBottom(props) {
   const [ref] = usePlane(() => ({position: [0, -4, 4], rotation: [-Math.PI / 2, 0, 0]}))
+  const [developerMode] = useDeveloperMode()
 
-  return (<mesh
-    {...props}
-    ref={ref}
-  >
-    <planeGeometry args={[8, 8]}/>
-    <meshPhongMaterial color={'blue'}/>
-  </mesh>)
+  return (
+    <mesh{...props} ref={ref}>
+      <planeGeometry args={[8, 8]}/>
+      <meshPhongMaterial color={developerMode ? 'blue' : null}/>
+    </mesh>
+  )
 }
 
 function BarrierLeft(props) {
   const [ref] = usePlane(() => ({position: [-4, 0, 4], rotation: [0, Math.PI / 2, 0]}))
+  const [developerMode] = useDeveloperMode()
 
-  return (<mesh
-    {...props}
-    ref={ref}
-  >
-    <planeGeometry args={[8, 8]}/>
-    <meshPhongMaterial color={'green'}/>
-  </mesh>)
+  return (
+    <mesh{...props} ref={ref}>
+      <planeGeometry args={[8, 8]}/>
+      <meshPhongMaterial color={developerMode ? 'green' : null}/>
+    </mesh>
+  )
 }
 
 function BarrierRight(props) {
   const [ref] = usePlane(() => ({position: [4, 0, 4], rotation: [0, -Math.PI / 2, 0]}))
+  const [developerMode] = useDeveloperMode()
 
-  return (<mesh
-    {...props}
-    ref={ref}
-  >
-    <planeGeometry args={[8, 8]}/>
-    <meshPhongMaterial color={'green'}/>
-  </mesh>)
+  return (
+    <mesh{...props} ref={ref}>
+      <planeGeometry args={[8, 8]}/>
+      <meshPhongMaterial color={developerMode ? 'green' : null}/>
+    </mesh>
+  )
 }
