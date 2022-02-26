1dbbde18044f2d907d65f79013e52e673ce6ed1e
Marco Li <marco_li@paradise-soft.com.tw>
2022-02-27 01:55:02 +0800

    feat(lab): 新增 three base

--- src/apps/lab/pages/_app.js
+++ src/apps/lab/pages/_app.js
@@ -1,5 +1,24 @@
+import Head from 'next/head'
+import {CssBaseline} from '@mui/material'
+import React from 'react'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>兩大類的實驗室</title>
+        <meta name="description" content="兩大類的實驗室"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <CssBaseline/>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/apps/lab/pages/index.js
+++ src/apps/lab/pages/index.js
@@ -1,22 +1,7 @@
 import React from 'react'
-import Head from 'next/head'
-import {CssBaseline} from '@mui/material'
 
 export default function Index() {
   return (
-    <>
-      <Head>
-        <title>實驗室</title>
-        <meta name="description" content="實驗室"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-        <link rel="icon" href="/favicon.ico"/>
-      </Head>
-
-      <CssBaseline/>
-
-      <div>兩大類的實驗室</div>
-    </>
+    <div>兩大類的實驗室</div>
   )
 }
