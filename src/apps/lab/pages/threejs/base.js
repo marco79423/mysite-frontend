97ea14a92638858971b45a961e0601266519dac0
Marco Li <marco_li@paradise-soft.com.tw>
2022-02-27 18:03:17 +0800

    feat(lab): 整理基礎範例

--- src/apps/lab/pages/threejs/base.js
+++ src/apps/lab/pages/threejs/base.js
@@ -1,25 +1,33 @@
 import React from 'react'
 import * as THREE from 'three'
+import Head from 'next/head'
 
 export default function Base() {
 
   React.useEffect(() => {
     const container = document.getElementById('container')
 
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-
-    const renderer = new THREE.WebGLRenderer()
+    // 設定 Renderer
+    const renderer = new THREE.WebGLRenderer({antialias: true})
+    renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight)
     container.appendChild(renderer.domElement)
 
-    const geometry = new THREE.BoxGeometry()
-    const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-    const cube = new THREE.Mesh(geometry, material)
+    // 建立場景
+    const scene = new THREE.Scene()
+
+    // 建立相機
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.set(0, 0, 5)
+    camera.lookAt(new THREE.Vector3(0, 0, 0))
+
+    // 建立模型、材質並放置到場景
+    const cubeGeometry = new THREE.BoxGeometry()
+    const cubeMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00})
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
     scene.add(cube)
 
-    camera.position.z = 5
-
+    // 繪製循環
     const animate = function () {
       cube.rotation.x += 0.01
       cube.rotation.y += 0.01
@@ -33,6 +41,12 @@ export default function Base() {
   }, [])
 
   return (
-    <div id="container"/>
+    <>
+      <Head>
+        <title>Base | Three.js | 兩大類的實驗室</title>
+      </Head>
+
+      <div id="container"/>
+    </>
   )
 }
